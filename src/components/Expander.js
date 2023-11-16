aa26d812651f0882cc5be5f3e1633039b68467a0
Van Albert <motorhobo@gmail.com>
2023-11-16 12:26:20 -0500

    merging accordionToExpander to master

---
+++ src/components/Expander.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import { useState, useRef, useEffect } from 'react'
+import Image from 'next/image'
+import { FaRegMinusSquare } from 'react-icons/fa'
+import { FaRegPlusSquare } from 'react-icons/fa'
+
+import SeparatorDots from './SeparatorDots'
+import RedDownArrow from '../../public/images/img-red-arrow-down.png'
+import RedRightArrow from '../../public/images/img-red-arrow-right.png'
+import GoldDownArrow from '../../public/images/img-gold-down-arrow.png'
+
+import * as styles from '../styles/Expander.module.scss'
+
+// !VA Get the props from the child
+const Expander = ({
+  title,
+  text,
+  children,
+  index,
+  activeExpanderIndex,
+  setActiveExpanderIndex,
+}) => {
+
+  const [maz, setMaz] = useState('')
+
+  // !VA 
+  const expanderRef = useRef()
+
+  const handleSetIndex= (index) => {
+    if (activeExpanderIndex !== index ) {
+      setActiveExpanderIndex(index);
+      // console.log('LATER activeExpanderIndex :>> ' + activeExpanderIndex);
+      setMaz('blob_' + index)
+
+    } else {
+      setActiveExpanderIndex(0);
+    }
+    // !VA Once the Expander content has expanded, we can move the top of the Expander container to the top of the screen with the scroll-margin set in the CSS. If you scrollIntoView before the Expander is expanded, the scroll operation will run first and then when the Expander opens it will scroll again to accommodate the new container.
+    // !VA expanderRef -> expanderRef
+    setTimeout(() => {
+      expanderRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }, 300);
+  }
+
+  console.log('styles :>> ');
+  console.log(styles);
+  console.log('maz :>> ');
+  console.log(maz);
+
+
+  return (
+    <>
+      <div className={styles.main}
+        ref={expanderRef}
+        >
+
+
+          <div
+            onClick={() => handleSetIndex(index)}
+            className={styles.block}
+            >
+            <div className={styles.head}>
+
+              <div className={styles.head_header}>
+                <div className={styles.head_header_icon}>
+                  {activeExpanderIndex === index ? (
+                    <Image 
+                      src={RedDownArrow} 
+                      width="0"
+                      height="0"
+                      sizes="100vw"
+                      // style={{ width: '40px', height: 'auto' }}
+                      className={styles.head_header_icon_right} alt="XXX" 
+                    />
+                  ) : (
+                    <Image 
+                      src={RedRightArrow} 
+                      width="0"
+                      height="0"
+                      sizes="100vw"
+                      // style={{ width: '12px', height: 'auto' }}ddd
+                      className={styles.head_header_icon_down} alt="XXX" 
+                    />
+                  )}
+                </div>
+                <h3 className={styles.head_header_heading}>{title}
+                </h3>
+              </div>
+
+              <div className={styles.head_content}>
+                <p className={styles.head_content_text}>
+                  {text}
+                </p>
+                <div className={`${styles.head_content_learnmore} ${activeExpanderIndex === index ? styles.hide_learnmore : ''}`}>
+                  <h4 className={styles.head_content_learnmore_text}>
+                    Learn More
+                  </h4>
+                  <Image 
+                    src={GoldDownArrow} 
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    // style={{ width: '100%', height: 'auto' }}
+                    className={styles.head_content_learnmore_icon} alt="Learn more..." 
+                  />
+                </div>
+              </div>
+            </div>
+
+            {activeExpanderIndex === index && (
+              <div 
+                className={styles.content}
+              >
+                {children}
+  
+              </div>
+            )}
+          </div>
+          {index !== 3 ? <hr className={styles.separator} /> : null}
+
+
+        
+      </div>
+    </>
+  )
+}
+
+export default Expander
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import HomeContentBlocks from '../components/HomeContentBlocks'
-import AccordionContent from '../components/AccordionContent'
+import Expander from '../components/Expander'
 import { FaPaw } from 'react-icons/fa'
 
 import Inspire from '../../public/images/img-inspire-home.png'
@@ -23,34 +23,34 @@ import * as styles from '../styles/Pages.module.scss'
 function Home() {
 
   // const [activeIndex, setActiveIndex] = useState(null) 
-  const [activeAccordionIndex, setActiveAccordionIndex] = useState(0)
+  const [activeExpanderIndex, setActiveExpanderIndex] = useState(0)
 
   return (
     <>
-      <section className={[styles.home_bg, 'styles.section'].join(' ')}>
+      {/* <section className={[styles.home_bg, 'styles.section'].join(' ')}> */}
+      <section className={`${styles.home_bg} ${styles.section}` }>
 
-      <AccordionContent
+        <div className={styles.home_expander}>
+
+          <Expander
             className='accordion__faqs'
             title='inspire'
-            text='Live performance based on one of the most inspirational stories in American&nbsp;history.'
+            text='Live music from one of the most inspirational stories in American&nbsp;history.'
             index={1}
-            activeAccordionIndex={activeAccordionIndex}
-            setActiveAccordionIndex={setActiveAccordionIndex}
+            activeExpanderIndex={activeExpanderIndex}
+            setActiveExpanderIndex={setActiveExpanderIndex}
             >
-            <p className='accordion__text'>About 100 years ago, a handful of musicians from the most impoverished and disadvantaged region in America created a sound that changed the course of popular music all over the world.  </p>
-            <div className="modal-icon-block">
-              <div className="modal-icon-block-icons">
-              </div>
-            </div>
-          </AccordionContent> 
+            <p className={styles.content_text}>About 100 years ago, a handful of musicians from the most impoverished and disadvantaged region in America created a sound that changed the course of popular music all over the world.</p>
 
-          <AccordionContent
+          </Expander> 
+
+          <Expander
                 className='accordion__faqs'
                 title='educate'
                 text='Image-based storytelling of people and their achievements over five eras.'
                 index={2}
-                activeAccordionIndex={activeAccordionIndex}
-                setActiveAccordionIndex={setActiveAccordionIndex}
+                activeExpanderIndex={activeExpanderIndex}
+                setActiveExpanderIndex={setActiveExpanderIndex}
                 >
                 <p className='accordion__text'>
                   Nobody knows for sure, but we agree with people who say the actual numbers are much higher than generally assumed. Some people say 1000 cases annually in the US, some say 5000. We think it&rsquo;s a lot more, because statistics indicate most people with LarPar dogs choose euthanasia over expensive tie-back surgery and that&rsquo;s the end of it. So there are untold thousands of cases in the &lsquo;dark zone&rsquo; &mdash; there&rsquo;s no way to get any data about why the dog was euthanized because there was no confirmed diagnosis. We&rsquo;ll have a blog post about this topic soon.
@@ -58,40 +58,20 @@ function Home() {
                   <noscript>
                   Nobody knows for sure, but we agree with people who say the actual numbers are much higher than generally assumed. Some people say 1000 cases annually in the US, some say 5000. We think it&rsquo;s a lot more, because statistics indicate most people with LarPar dogs choose euthanasia over expensive tie-back surgery and that&rsquo;s the end of it. So there are untold thousands of cases in the &lsquo;dark zone&rsquo; &mdash; there&rsquo;s no way to get any data about why the dog was euthanized because there was no confirmed diagnosis. We&rsquo;ll have a blog post about this topic soon.
                   </noscript>
-          </AccordionContent> 
+          </Expander> 
 
-          <AccordionContent
+          <Expander
             className='accordion__faqs'
             title='engage'
             text='Engaging audiences on multiple levels to help them stay focused.'
             index={3}
-            activeAccordionIndex={activeAccordionIndex}
-            setActiveAccordionIndex={setActiveAccordionIndex}
+            activeExpanderIndex={activeExpanderIndex}
+            setActiveExpanderIndex={setActiveExpanderIndex}
             >
             <p className='accordion__text'>If you don&rsquo;t do anything, your dog&rsquo;s condition will worsen and he or she will eventually not <be></be> able to get enough air to sustain life. There&rsquo;s no telling how long this could take. In our case, our dog was 11 before the first symptoms appeared, and it was 2&frac12; years before she reached end-stage. The first 18 months were manageable, and it wasn&rsquo;t until the last two months that we started asking ourselves whether it was time for us to let her go. But your case may be different so we can only say this: if your dog has early stage laryngeal paralysis, there&rsquo;s not much you can do except prepare yourself for what&rsquo;s to come. You must have a game plan for when it starts affecting your dog&rsquo;s quality of life. Because if your dog stays otherwise healthy, the laryngeal paralysis <span className='italic'>will</span> progress and you <span className='italic'>will</span> eventually have to take some action.</p>
-          </AccordionContent> 
+          </Expander> 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </div>
 
       </section>
       {/* <section className='landing'>
