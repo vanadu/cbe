2ade71156e4f385d991c44834e09a049bb72edcf
Van Albert <motorhobo@gmail.com>
2024-01-31 17:28:06 -0500

    Fixed contact page response

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -41,12 +41,10 @@ export const Contact = () => {
           console.log(error.text)
         }
       )
+
   }
 
-  // const handleClose = (e) => {
-  //   console.log('handleClose');
-  // }
-  
+
 
   return (
     <>
@@ -63,59 +61,88 @@ export const Contact = () => {
           <hr className={styles.page_head_rule}/>
         </div>
       
-        <form 
-          className={styles.contact}
-          ref={form} 
-          onSubmit={sendEmail}
-          >
-          {/* <div className="close-box"><Link to="/"><FaRegWindowClose /></Link></div> */}
-          <div className={styles.contact_head}>
-            <h2 className={styles.contact_head_heading}>Contact Us</h2>
-            <p className={styles.contact_head_text}>
-              Our program is the perfect complement for your school or church &mdash; or any other venue that brings adults and kids together. To find out more about <span className={styles.semibold}>Crossroads Blues Experience</span>, please feel free to send us a message. 
-            </p>
-          </div>
+
           <div className={styles.contact_feedback}>
-            {resText && <p className={styles.contact_feedback_text}>Thanks for your message. You&rsquo;ll hear from us soon.</p>}
-            {resError && <p className={styles.contact_feedback_text}>Your message wasn&rsquo;t sent. Maybe try again later?</p>}
-          </div>
-          <div className={styles.contact_body}>
+            {!resText && 
+                <>
+
+                <form 
+                  className={styles.contact}
+                  ref={form} 
+                  onSubmit={sendEmail}
+                  >
+                  {/* <div className="close-box"><Link to="/"><FaRegWindowClose /></Link></div> */}
+                  <div className={styles.contact_head}>
+                    <h2 className={styles.contact_head_heading}>Contact Us</h2>
+                    <p className={styles.contact_head_text}>
+                      Our program is the perfect complement for your school, church or any other venue that brings adults and kids together. To find out more about <span className={styles.semibold}>Crossroads Blues Experience</span>, please feel free to send us a message. 
+                    </p>
+                  </div>
+
+
+                  <div className={styles.contact_body}>
               
-            <div className={styles.contact_info}>
-              <div className={styles.contact_input}>
-                {/* <label className='block text-gray-700 text-sm font-bold mb-2'>Your name:</label> */}
-                <label className={styles.contact_input_label}>Your name:</label>
-                <input 
-                  className={styles.contact_input_text}
-                  type='text' 
-                  name='user_name'
-                  onChange={ (e) => setName(e.target.value)}
-                  value={ name }
+              <div className={styles.contact_info}>
+                <div className={styles.contact_input}>
+                  {/* <label className='block text-gray-700 text-sm font-bold mb-2'>Your name:</label> */}
+                  <label className={styles.contact_input_label}>Your name:</label>
+                  <input 
+                    className={styles.contact_input_text}
+                    type='text' 
+                    name='user_name'
+                    onChange={ (e) => setName(e.target.value)}
+                    value={ name }
+                    />
+                </div>
+                <div className={styles.contact_input}>
+                  <label className={styles.contact_input_label}>Your email:</label>
+                  <input 
+                    className={styles.contact_input_text}
+                    type='email' 
+                    name='user_email'
+                    onChange={ (e) => setEmail(e.target.value)}
+                    value={ email }
                   />
+                </div>
               </div>
-              <div className={styles.contact_input}>
-                <label className={styles.contact_input_label}>Your email:</label>
-                <input 
-                  className={styles.contact_input_text}
-                  type='email' 
-                  name='user_email'
-                  onChange={ (e) => setEmail(e.target.value)}
-                  value={ email }
+              <div className={styles.contact_message}>
+                <label className={styles.contact_input_label}>Message:</label>
+                <textarea 
+                className={styles.contact_message_text}
+                onChange={ (e) => setMessage(e.target.value)}
+                value={ message }
                 />
               </div>
+              {/* <button className='contact_button bg-blue-900 block mx-auto px-12 py-4 w-[140px] text-white mt-12' type='submit'>SEND</button> */}
+              <button className={styles.contact_button} type='submit'>SEND</button>
             </div>
-            <div className={styles.contact_message}>
-              <label className={styles.contact_input_label}>Message:</label>
-              <textarea 
-              className={styles.contact_message_text}
-              onChange={ (e) => setMessage(e.target.value)}
-              value={ message }
-              />
-            </div>
-            {/* <button className='contact_button bg-blue-900 block mx-auto px-12 py-4 w-[140px] text-white mt-12' type='submit'>SEND</button> */}
-            <button className={styles.contact_button} type='submit'>SEND</button>
+          </form>
+
+
+
+
+                </>
+            }
+            {resText && 
+            <>
+                {resText && 
+                  <div className={styles.feedback_success}>
+                    <h3 className={styles.feedback_success_heading}>Your message was sent successfully.</h3>
+                    <p className={styles.contact_feedback_text}>Thanks for contacting us at <span className={styles.semibold}>Crossroads Blues Experience</span>. We&rsquo;ll respond as soon as we can. You might get a faster response by sending us a PM on <Link href="https://www.facebook.com/crossroadsbluesexperience" className={styles.link_secondary}>our Facebook page</Link>.</p>
+                  </div>
+                }
+            
+            
+            
+            
+            
+            </>
+            }
+            {resError && <p className={styles.contact_form_feedback_text}>Your message wasn&rsquo;t sent. Maybe try again later?</p>}
+
+
           </div>
-        </form>
+
 
       </section>
     </>
