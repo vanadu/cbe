05cd7ae1130762f6840a5ab357d09cf296b5c929
Van Albert <motorhobo@gmail.com>
2024-08-19 12:30:38 -0400

    2024.08.19A - First build of updated website

--- src/pages/program.js
+++ src/pages/program.js
@@ -113,20 +113,45 @@ const Program = () => {
               Our Mission
             </h2>
             <p className={styles.content_text}>
-              Our mission is to inspire, educate and engage students with a multimedia experience interweaving live music, geography, social studies, and history based on the narrative of the origins of blues music in the Mississippi Delta at the beginning of the 20th century. Founded on the concept of the Crossroads as intersection point between cultures, backgrounds and generations, <span className={styles.semibold}>Crossroads&nbsp;Blues&nbsp;Experience</span> aims to empower young people to achieve their innate potential.
+              Our mission is to inspire, educate and engage students with a multimedia experience interweaving live music, geography, social studies, and history based on the narrative of the origins of blues music in the Mississippi Delta at the beginning of the 20th century.
             </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more..'
+                index={3}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    Founded on the concept of the Crossroads as intersection point between cultures, backgrounds and generations, <span className={styles.semibold}>Crossroads&nbsp;Blues&nbsp;Experience</span> aims to empower young people to achieve their innate potential.
+                  </p>
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    The messaging of our program is clear &mdash; no matter what your background or heritage, if you find something you love to do and work at it, you can make a difference in people&lsquo;s lives &mdash; and maybe even change the world.
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
 
             <h2 className={styles.page_heading}>
               Program Structure
             </h2>
             <p className={styles.content_text}>
-              The Crossroads Blues Experience program performs a series of iconic American songs, each of which represents an era or pivotal event in American history. Each song is accompanied with a multimedia show that presents the historical progression of that era (i.e., walking, steamboats, trains, automobiles, and air travel) or imagery from that event. All of these eras experienced a transition nexus sometime around 100 years ago, as blues music was also in its formative years. These overlapping chronologies are reinforced with multimedia imagery as the program progresses through history. 
+              The Crossroads Blues Experience program comprises a series of iconic American songs, each of which represents an era associated with a mode of transportation or a pivotal event in American history. Each song is accompanied with a multimedia show that presents the historical progression of that era or imagery from that event.
+            </p>
+            <p className={styles.content_text}>
+              Each of these eras experienced a transformation sometime around 100 years ago as our society transitioned from the coal-driven steam engine to liquid fossil fuels and the internal-combustion engine. At that time, blues music was also in its formative years. Our program reinforces these overlapping timelines with multimedia imagery to help students visualize and conceptualize the progressive chronology we call <span className={styles.italic}>history</span>. 
             </p>
             {/* ShowMoreComponent content start */}
             <div className={styles.content_text}>
               <ShowMoreContent
                 title='Learn more...'
-                index={3}
+                index={4}
                 activeIndex={activeIndex}
                 setActiveIndex={setActiveIndex}
                 >
@@ -141,31 +166,160 @@ const Program = () => {
             {/* ShowMoreComponent content end */}
             
             <h3 className={styles.page_subheading}>
-              Things You Can Do If You&lsquo;re You
+              I. Introduction
             </h3>
             <p className={styles.content_text}>
-              Our program opens with graphical picturebook video that tells the story of how a handful of musicians from the most disadvantaged, underdeveloped region of the country created a sound that changed the course of popular music all over the world.  
+              Our program opens with an inspirational short (4-minute) video entitled <span className={styles.italic}>Things You Can Do If You&lsquo;re You</span>.   
             </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more...'
+                index={5}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    People in the Smartphone Generation are accustomed to fast-paced graphics and image-based storytelling. In this way, our introductory video captures students&lsquo; attention and imagination to tell the story of how a handful of musicians from the most disadvantaged, underdeveloped region of the country created a sound that changed the course of popular music all over the world. 
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
             <h3 className={styles.page_subheading}>
-              Walking Blues (Son House) &amp; Backwater Blues (Bessie Smith)
+              I. Walking Blues (Son House) &amp; Backwater Blues (Bessie Smith)
             </h3>
             <p className={styles.content_text}>
-              Walking Blues provides the live soundtrack as we lead students through a brief history of photography so they can better understand the images that follow. Then, we take a stroll through the Mississippi Delta, the birthplace of blues music, about 100 years ago when for most people walking was the only means of transportation. With Backwater Blues, we introduce our audience to the Great Mississippi Flood of 1927 and its far-reaching consequences.   
+              The Mississippi Delta has the distinction of being both the birthplace of blues as well as the most economically depressed region in America.    
+            </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more...'
+                index={6}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                  <span className={styles.italic}>Walking Blues</span> provides the live soundtrack as we lead students through a brief history of photography so they can better understand the images that follow. Then, we take a stroll through the Mississippi Delta about 100 years ago, when for most people walking was the only means of transportation. 
+                  </p>
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    Our visit to the Mississippi Delta continues with the Great Mississippi Flood of 1927. Arguably America&lsquo;s most devastating natural disaster, 1927 was the first time photography played a large-scale critical role in the relief and rescue effort. The multimedia imagery captures the magnitude of the devastation, accompanied by the haunting live rendition of Bessie Smith&lsquo;s blues classic <span className={styles.italic}>Backwater Blues</span>. 
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
+            <p className={styles.content_text}>
+            
             </p>
 
             <h3 className={styles.page_subheading}>
-              Down By The Riverside (African-American Spiritual)
+              II. Down By The Riverside (African-American Spiritual)
             </h3>
             <p className={styles.content_text}>
-              The Great Mississippi and the Steamboat Era are inextricably linked in our national consciousness. With this song, we begin our journey on the Mississippi and follow the steam engine through its impact on maritime history.
+              The Great Mississippi River and the Steamboat Era are inextricably linked in our national consciousness.
             </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more...'
+                index={7}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    We begin our journey on the Mississippi and follow steam engine through the course of its development and impact on maritime history, from the earliest steamboat in 1797 through to the last steam-powered American aircraft carrier in 1968. Students can sing along to the well-known spiritual <span className={styles.italic}>Down By The Riverside</span>, performed in the authentic Piedmont blues style.  
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
             <h3 className={styles.page_subheading}>
-              This Train (African-American Spiritual)
+              III. This Train (African-American Spiritual)
             </h3>
             <p className={styles.content_text}>
-              The steam locomotive brought about the end of
-               the Riverboat Era. The imagery accompanying this song takes us through the rise of trains to transportation preeminence, the building of the railroads and transformation in the 20th century.  
+              While the steam locomotive did bring about the end of the Riverboat Era, the transition to the Railroad Era took many decades. 
             </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more...'
+                index={8}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    The steam locomotive brought many engineering challenges that steamboats weren&lsquo;t subject to, in particular with regard to boiler safety and access to the water required to drive steam engines across land. We guide students through the amazing history of the Railroad Era in images and song, accompanied by an live singalong performance of the well-known spiritual <span className={styles.italic}>This Train</span> played in the authentic Delta Blues style. 
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
+            <h3 className={styles.page_subheading}>
+              IV. Key to the Highway (Big Bill Broonzy)
+            </h3>
+            <p className={styles.content_text}>
+              In the extended program for stage venues, we continue our journey with the arrival of the Automobile Age and Thomas Edison&lsquo;s fleet of electric taxis at the beginning of the 20th century.
+            </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+              <ShowMoreContent
+                title='Learn more...'
+                index={9}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    The electic vehicle, however, was no match for liquid fossil fuels and the internal combustion engine. By the 1920s, gas-powered automobiles were already supplanting the railroad as the world&lsquo;s primary people-mover. Like many people in the American South, blues music, too, packed up and headed north. In this section, students can follow the development of the car, accompanied by an unplugged Chicago version of Big Bill Broonzy&lsquo;s classic <span className={styles.italic}>Key to the Highway</span>. 
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+
+            <h3 className={styles.page_subheading}>
+              V. I&lsquo;ll Fly Away (Albert E. Brumley)
+            </h3>
+            <p className={styles.content_text}>
+              Nothing will give you the blues faster than building an airplane that doesn&lsquo;t fly.
+            </p>
+            {/* ShowMoreComponent content start */}
+            <div className={styles.content_text}>
+
+              <ShowMoreContent
+                title='Learn more...'
+                index={10}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                >
+                <div className="showmore-content-text">
+                  <span className="showmore-content-space"></span>
+                  <p className="showmore-content-text">
+                    By the time of the arrival of the Jet Age, blues music had been supplanted on the American popular music landscape by rock-and-roll and eventually what became known as classic rock music. Blues music was rooted in the American South of the early- to mid-20th century, so airplanes never took a prominent role in blues songs like trains or riverboats did. But that doesn&lsquo;t mean the influence of blues music doesn&lsquo;t live on. Our extended program closes with <span className={styles.italic}>I&lsquo;ll Fly Away</span>, a singalong tribute played in the Piedmont style to the men and women who pioneered flight &mdash; some successfully, some not. 
+                  </p>
+                </div>
+              </ShowMoreContent>
+            </div>  
+            {/* ShowMoreComponent content end */}
+            
+
+            
           </div>
 
         </motion.div>
--- src/pages/teach.js
+++ src/pages/teach.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import HeadBrace from '../../public/images/img-head-brace.jpg'
@@ -9,6 +10,7 @@ import ShowMoreContent from '../components/ShowMoreContent'
 import * as styles from '../styles/Pages.module.scss'
 
 const Teach = () => {
+  const [activeIndex, setActiveIndex] = useState(null) 
   return (
       <>
         {/* <section className={[styles.home_bg, 'styles.section', out_copy].join(' ')}> */}
@@ -27,21 +29,99 @@ const Teach = () => {
               </div>
               <h2 className={styles.page_heading}>Assets for Teachers</h2>
               <p className={styles.content_text}>
-                In addition to the in-class/onstage performance, Crossroads Blues Experience is currently developing supplemental online assets that teachers can use to build on the learning framework established during the live music/multimedia performance. These assets can help teachers initiate deep learning by reinforcing ideas and information from the program and encouraging students to ask questions and create their own stories.
+               Crossroads Blues Experience is currently developing supplemental online assets that teachers can use to build on the learning framework established during the live music/multimedia performance. These assets can help teachers initiate deep learning by reinforcing ideas and information from the program and encouraging students to ask questions and create their own stories.
               </p>
-
-
+              <h3 className={styles.page_subheading}>
+                What&lsquo;s the Story Behind This Picture?
+              </h3>
               <Image 
                   src={HeadBrace} 
                   width="0"
                   height="0"
                   sizes="100vw"
                   // style={{ width: '100%', height: 'auto' }}
-                  className={styles.figure_two_up_image} alt="Acme Guitars" 
+                  className={styles.figure_full_width} alt="Acme Guitars" 
                 />
-  
-  
-  
+              <p className={styles.content_text}>
+                This picture sparks a lot of curiosity during our performances and provides a wealth of learning moments to stimulate discussion and creative action. Teachers can start by asking students to create a story around this picture without knowing anything about it. They can then ask directed questions that help students discover the real story behind the image. 
+              </p>
+              {/* ShowMoreComponent content start */}
+              <div className={styles.content_text}>
+                <ShowMoreContent
+                  title='Are these men twins or is it the same person?'
+                  index={1}
+                  activeIndex={activeIndex}
+                  setActiveIndex={setActiveIndex}
+                  >
+                  <div className="showmore-content-text">
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                     It&lsquo;s the same person. This is an early example of &lsquo;trick&rsquo; photography. It is actually two separate photos taken in the same room with the same subject in two different positions. 
+                    </p>
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      <span className={styles.semibold}>For Discussion</span>: A picture that combines elements from multiple images is called a <span className={styles.italic}>composite</span> image. Can you use your phone to make a composite image that shows you in two different places in the same picture? How can you use software to create a composite image? How is the process different using software compared to how it was done back then?
+                    </p>
+                  </div>
+                </ShowMoreContent>
+              </div>  
+              {/* ShowMoreComponent content end */}
+
+              {/* ShowMoreComponent content start */}
+              <div className={styles.content_text}>
+                <ShowMoreContent
+                  title='What&lsquo;s that thing behind the head of the man on the left?'
+                  index={2}
+                  activeIndex={activeIndex}
+                  setActiveIndex={setActiveIndex}
+                  >
+                  <div className="showmore-content-text">
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      Modern cell phone cameras have very sensitive digital light sensors. That means they don’t need much light to capture an image. The shutter on your phone’s camera is only open for one five-hundredth of a second or less and still captures enough light to create a clear picture.
+                    </p>
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      But in the early days of photography, images were captured by exposing chemicals on a photographic plate to light. These chemicals were not very light-sensitive at all. In early cameras, the shutter had to be open for 30 seconds to capture enough light for the chemical reaction required to create the image.
+                    </p>
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      That meant that the subject had to be perfectly still for up to 30 seconds while the picture was being taken. Any movement at all could make the picture blurry or even unrecognizable. Photographers often used braces behind the subjects head to help them remain motionless while the picture was being taken.
+                    </p>
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      <span className={styles.semibold}>For Discussion:</span> How hard is it to stay completely motionless for 30 seconds without moving your face at all or even blinking? Can anyone do it? Why do early photos rarely show people smiling? Can you hold a smile on your face for thirty seconds? How does a camera&lsquo;s shutter work? How can you change the shutter speed on your cell phone camera and what happens to the pictures if you do? What other settings on your phone&lsquo;s camera can you change and how do they affect your pictures?   
+                    </p>
+                  </div>
+                </ShowMoreContent>
+              </div>  
+              {/* ShowMoreComponent content end */}
+
+              {/* ShowMoreComponent content start */}
+              <div className={styles.content_text}>
+                <ShowMoreContent
+                  title='What do our eyes see when things move very fast?'
+                  index={3}
+                  activeIndex={activeIndex}
+                  setActiveIndex={setActiveIndex}
+                  >
+                  <div className="showmore-content-text">
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      When a camera shutter is open, the camera captures everything happening in the scene. If something is moving while the shutter is open, that motion is captured, too, and the moving object appears as a blur. We see the same effect with our eyes when something moves very fast within our field of vision.
+                    </p>
+                    <span className="showmore-content-space"></span>
+                    <p className="showmore-content-text">
+                      <span className={styles.semibold}>For Discussion:</span> What do you see when you wave your hand back and forth in front of your face really fast? How about when you look at an electric rotary fan? Do you see the individual fan blades or do all the fan blades blend together? How is this like using a very slow shutter speed on your phone&lsquo;s camera? Have you ever made a snow angel? How is that similar to how our eyes perceive very fast motion?   
+                    </p>
+                  </div>
+                </ShowMoreContent>
+              </div>  
+              {/* ShowMoreComponent content end */}
+              <h3 className={styles.page_heading}>About The Images</h3>
+              <p className={styles.content_text}>
+                All the images used in the <span className={styles.semibold}>Crossroads Blues Experience</span> program were curated for historical significance and suitability as learning aids in the classroom. As we collect feedback from our audiences moving forward, we will be expanding the teaching materials available on our website.
+              </p>
             </div>       
   
   
